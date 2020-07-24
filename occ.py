e6ecff5a1251c468c0103912e3a5d86811124a59
Dongpin <dhehdqls@gmail.com>
2020-07-24 16:44:55 +0000

    Add other models.

--- occ.py
+++ occ.py
@@ -12,14 +12,29 @@ import matplotlib
 import scipy
 import pandas as pd
 import random
+import sys
+
+from abc import *
+
 from scipy import io
+
 from sklearn.model_selection import train_test_split as train_test_split
 from sklearn.decomposition import PCA as PCA
 from sklearn.decomposition import KernelPCA as kPCA
 from sklearn.preprocessing import Normalizer
 from sklearn.base import BaseEstimator, ClassifierMixin
+
+from pyod.models.iforest import IForest
+from pyod.models.lof import LOF
+from pyod.models.ocsvm import OCSVM
+from pyod.models.knn import KNN
+from pyod.models.hbos import HBOS
+from pyod.models.lscp import LSCP
+from pyod.models.auto_encoder import AutoEncoder
+from pyod.models.vae import VAE 
+from suod.models.base import SUOD
+
 matplotlib.style.use("ggplot")
-#tf.__version__
 tf.test.is_gpu_available()
 #gpus = tf.config.experimental.list_physical_devices('GPU')
 
@@ -73,26 +88,37 @@ class occ():
             data = occ.sampling_X(self.X, rate=sampling)
         kernel_set = 'poly'
         gamma_set = 'scale'
-        epoches = 10
+        epochs = 10
         batch_size = 50
-        nu = 0.05
+        nu = 0.5
+        hidden_neurons = [64,32,32,64]
         
         for k, v in kwargs.items():
             if 'kernel' == k:
                 kernel_set = v
             if 'gamma' == k:
                 gamma_set = v
-            if 'epoches' == k:
-                epoches = v
+            if 'epochs' == k:
+                epochs = v
             if 'nu' == k:
                 nu = v
             if 'batch_size' == k:
                 batch_size = v
+            if 'hidden_neurons' == k:
+                hidden_neurons = v
                 
         if model=='ocsvm':
             self.model = sklearn.svm.OneClassSVM(gamma=gamma_set, kernel=kernel_set, nu=nu)
         elif model=='ocnn':
-            self.model = ocnn(len(data[0]), epoches=epoches, nu=nu, batch_size=batch_size)
+            self.model = ocnn(len(data[0]), epochs=epochs, nu=nu, batch_size=batch_size)
+        elif model == 'ensemble':
+            self.model = ensemble(nu=nu)
+        elif model == 'isolationForest':
+            self.model = sklearn.ensemble.IsolationForest(contamination=nu)
+        elif model == 'autoEncoder':
+            self.model = AutoEncoderOOD(nu=nu, hidden_neurons=hidden_neurons, epochs=epochs)
+        elif model == 'vae':
+            self.model = VAE_ODD(nu=nu, hidden_neurons=hidden_neurons, epochs=epochs)
         else:
             print("There is no such model type {}".format(model))
         
@@ -219,8 +245,22 @@ class custom_hinge_loss(tf.keras.losses.Loss):
         return (1 / nu) * tf.keras.backend.mean(tf.keras.backend.maximum(0.0, r - y), axis=-1)
 
 
-class ocnn():
-    def __init__(self, input_dim, hidden_layer_size=64, batch_size=50, r=1.0, epoches=10, nu=0.10):
+class abstract_occ_model(metaclass=ABCMeta):
+    @abstractmethod
+    def fit(self, X):
+        pass 
+    @abstractmethod
+    def predict(self, X):
+        pass
+    
+    @abstractmethod
+    def score_samples(self, X):
+        pass
+    
+
+
+class ocnn(abstract_occ_model):
+    def __init__(self, input_dim, hidden_layer_size=64, batch_size=50, r=1.0, epochs=10, nu=0.10):
         """
         :param input_dim: number of input features
         :param hidden_layer_size: number of neurons in the hidden layer
@@ -233,7 +273,7 @@ class ocnn():
         self.hidden_size = hidden_layer_size
         self.r = r
         self.batch_size = batch_size 
-        self.epoches = epoches
+        self.epochs = epochs
         self.nu = nu 
         self.build_model()
     
@@ -249,7 +289,7 @@ class ocnn():
         :return: trained model and callback history
         """
         
-        self.opt = tf.optimizers.Adam(lr=init_lr, decay=init_lr / self.epoches)
+        self.opt = tf.optimizers.Adam(lr=init_lr, decay=init_lr / self.epochs)
         self.model.compile(optimizer=self.opt,
                       loss=self.loss, run_eagerly=True)
 
@@ -258,7 +298,7 @@ class ocnn():
         self.history = self.model.fit(X, np.zeros((X.shape[0],)),
                             batch_size=self.batch_size,
                             shuffle=True,
-                            epochs=self.epoches,
+                            epochs=self.epochs,
                             verbose=1)
         
     def predict(self, X, log=True):
@@ -300,3 +340,70 @@ class ocnn():
         loss_val = (1 / self.nu) * tf.keras.backend.mean(tf.keras.backend.maximum(0.0, self.r - y_pred), axis=-1)
         self.r = tfp.stats.percentile(tf.reduce_max(y_pred_prev, axis=1), 100 * self.nu)
         return loss_val
+    
+class ensemble(abstract_occ_model):
+    """
+    
+    """
+    def __init__(self, nu=0.1):
+        self.base_estimators = [
+            #OCSVM(contamination=nu),
+            
+            KNN(n_neighbors=100, contamination=nu),
+            KNN(n_neighbors=25, contamination=nu),
+            KNN(n_neighbors=5, contamination=nu),
+            IForest(contamination=nu)
+        ]
+        self.model = SUOD(base_estimators=self.base_estimators,
+                         rp_flag_global=False,
+                         bps_flag=True,
+                         approx_flag_global=False)
+        self.scores = None
+        
+    def fit(self, X):
+        self.model.fit(X)
+        self.model.approximate(X)
+        
+    def predict(self,X):
+        self.scores = self.compute_score(X)
+        return np.where(self.scores>=0.5, 1 , np.where(self.scores<0.5, -1, self.scores))
+    
+    def score_samples(self,X):
+        if type(self.scores) != np.ndarray:
+            self.scores = self.compute_score(X)
+            return self.scores
+        else:
+            return self.scores
+    
+    def compute_score(self,X):
+        mean_prob = np.mean(self.model.predict_proba(X), axis=1)
+        return mean_prob
+
+    
+class AutoEncoderOOD(abstract_occ_model):
+    def __init__(self, hidden_neurons, nu, epochs):
+        self.model = AutoEncoder(hidden_neurons=hidden_neurons, contamination=nu, epochs=epochs)
+        
+    def fit(self, X):
+        self.model.fit(X)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+    def score_samples(self, X):
+        return -self.model.decision_function(X)
+    
+class VAE_ODD(AutoEncoderOOD):
+    def __init__(self, hidden_neurons, nu, epochs):
+        if len(hidden_neurons) % 2 == 0:
+            print("The number of layers must be an odd number(2n+1).")
+            sys.exit()
+        encoder = hidden_neurons[0:len(hidden_neurons)//2]
+        latent = hidden_neurons[len(hidden_neurons)//2]
+        decoder = hidden_neurons[len(hidden_neurons)//2+1:len(hidden_neurons)]
+        self.model = VAE(encoder_neurons=encoder,
+                         decoder_neurons=decoder,
+                         latent_dim=latent,
+                         contamination=nu,
+                         epochs=epochs
+                        )
